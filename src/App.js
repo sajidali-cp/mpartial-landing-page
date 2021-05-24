fd94e639d7f10a8ac4f610410d6b158b79001e01
sajid ali <sajidalicodingpixel@gmail.com>
2021-05-25 03:21:09 +0500

    ground truth data (title section)

--- src/App.js
+++ src/App.js
@@ -6,11 +6,13 @@ import {
 } from "react-router-dom";
 import Header from "./components/Header/Header";
 import Home from "./components/Home/Home";
+import GroundTruthDataPage from "./pages/GroundTruthDataPage/GroundTruthDataPage";
 function App() {
   return (
     <Router>
       <Header />
       <Home />
+      <GroundTruthDataPage/>
     </Router>
   );
 }
